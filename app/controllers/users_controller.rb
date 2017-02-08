8ed71d54232d63993b7853fe52fd315d4208ae93
congago <tnngocthinh4@gmail.com>
2017-02-08 16:10:41 +0700

    admin_create_new account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,26 @@ class UsersController < ApplicationController
   def index
     @users = User.order_by_newest.page(params[:page]).per Settings.per_page.user
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "profile.created"
+      redirect_to root_url
+    else
+      flash[:error] = t "profile.create_fail"
+      render new_user_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :user_name, :email, :password,
+      :password_confirmation
+  end
 end
