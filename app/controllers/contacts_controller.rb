df9c9afdea06217aaa58b2ca34c83f118c905a4e
congago <tnngocthinh4@gmail.com>
2017-02-14 13:52:57 +0700

    add refactor contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,5 @@
 class ContactsController < ApplicationController
   def new
-    @contact = Contact.new
     @key = Settings.link_map + ENV["GOOGLE_API_KEY"]
   end
 
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+    @key = Settings.link_map + ENV["GOOGLE_API_KEY"]
     @projects = Project.order_by_newest.limit Settings.limit_project
   end
 end
