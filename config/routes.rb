ec4ea673593390204dc495063504d6e3f5d7e644
ptnk1995 <nguyenphuoctiendn@gmail.com>
2017-02-16 00:16:10 +0700

    multi-language

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {omniauth_callbacks: "callbacks",
     registrations: "users"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  scope "(:locale)", locale: /en|vi/ do
+  scope "(:locale)", locale: /en|vi|ja/ do
     root "static_pages#home"
     resources :projects do
       resources :users
