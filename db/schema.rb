b0eef1dbdb609d6dcaef118f288f3cb7440d3f50
ptnk1995 <nguyenphuoctiendn@gmail.com>
2017-02-19 20:07:25 +0700

    flash-message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214044735) do
+ActiveRecord::Schema.define(version: 20170217091341) do
 
   create_table "attachments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "path"
@@ -49,6 +49,18 @@ ActiveRecord::Schema.define(version: 20170214044735) do
     t.string   "image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "ckeditor_assets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "data_file_name",               null: false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.string   "type",              limit: 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.index ["type"], name: "index_ckeditor_assets_on_type", using: :btree
   end
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -108,13 +120,13 @@ ActiveRecord::Schema.define(version: 20170214044735) do
   end
 
   create_table "notifications", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_create_id"
     t.integer  "notificationable_id"
     t.string   "notificationable_type"
     t.string   "notice_type"
     t.boolean  "read",                  default: false
     t.boolean  "checked",               default: false
     t.integer  "user_id"
+    t.integer  "user_create_id"
     t.datetime "created_at",                            null: false
     t.datetime "updated_at",                            null: false
     t.index ["notificationable_id", "notificationable_type"], name: "fk_notificationables", using: :btree
@@ -204,6 +216,19 @@ ActiveRecord::Schema.define(version: 20170214044735) do
     t.datetime "updated_at"
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
     t.index ["name"], name: "index_roles_on_name", using: :btree
+  end
+
+  create_table "slides", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.string   "title_en"
+    t.string   "title_vi"
+    t.text     "caption_en", limit: 65535
+    t.text     "caption_vi", limit: 65535
+    t.string   "title_ja"
+    t.text     "caption_ja", limit: 65535
+    t.string   "image"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "socials", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
